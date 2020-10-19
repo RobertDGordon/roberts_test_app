44adde79dea586a8ee4b6fcf8363082937d24157
Robert Gordon <Komeuppance@gmail.com>
2020-10-18 23:41:30 -0700

    Added resource picker

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,24 @@
 import { Page } from "@shopify/polaris";
+import { ResourcePicker } from "@shopify/app-bridge-react";
 
-const Index = () => (
-  <Page
-    title='Product Selector'
-    primaryAction={{
-      content: 'Select Products',
-      onAction: () => console.log('been a while')
-    }}
-    
-  />
-);
+class Index extends React.Component {
+  state = { open: false };
+  render() {
+    return(
+        <Page
+          title='Product Selector'
+          primaryAction={{
+            content: 'Select Products',
+            onAction: () => this.setState({open: true})
+          }}
+        >
+        <ResourcePicker
+          resourceType="Product"
+          open={this.state.open}
+        />
+        </Page>
+    )
+  }
+}
 
 export default Index;
